377fb2166387024a43c9fbd3749e3139158a7b02
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-19 03:56:26 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -46,26 +46,26 @@ class BrowserManager:
             pass
         # finding the serach icon and field
         locator1 = "//button[@aria-pressed='false']//*[name()='svg']"
-        browser.wait_until_page_contains_element(locator1, timeout=10)
-        browser.click_element(locator1)
+        self.browser.wait_until_page_contains_element(locator1, timeout=10)
+        self.browser.click_element(locator1)
 
         # inserting the search phrase in the input field
-        browser.input_text("//input[@placeholder='Search']",phrase)
-        browser.click_button("//button[@aria-label='Search Al Jazeera']")
-        browser.close_all_browsers()
+        self.browser.input_text("//input[@placeholder='Search']",phrase)
+        self.browser.click_button("//button[@aria-label='Search Al Jazeera']")
+        self.browser.close_all_browsers()
 
 
         # Trying to find it there is a realated articles with the search phrase
         try:
             locator2 = "//select[@id='search-sort-option']"
-            browser.wait_until_element_is_visible(locator2, timeout=10)
-            browser.click_element(locator2)
+            self.browser.wait_until_element_is_visible(locator2, timeout=10)
+            self.browser.click_element(locator2)
         except:
             return "No news associated with the search phrase"
 
         # sort by time
         dropdown_locator = "//select[@id='search-sort-option']/option[1]" 
-        browser.click_element(dropdown_locator)
+        self.browser.click_element(dropdown_locator)
 
 bm = BrowserManager()
 url = "https://www.aljazeera.com/"
