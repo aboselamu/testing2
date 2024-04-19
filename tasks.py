495405ba03fd3ded2b48f279c677e08d519a3ab8
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-19 03:49:50 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,34 +1,73 @@
-from robocorp.tasks import task
-from robocorp import browser
-from robocorp.tasks import get_output_dir
 from RPA.Browser.Selenium import Selenium 
 
 
-@task
-def minimal_task():
-    # browser = Selenium(auto_close = False)
-    # browser.open_available_browser('https://www.google.co.uk')
-    browser = Selenium(auto_close = False)
-    browser.open_available_browser('https://www.google.co.uk')
-    try:
-        browser.click_button('Accept all')
-    except:
-        pass
-    browser.input_text('name:q','rpaframework')
-    browser.click_button('Google Search',"ENTER")
-    # browser.input_text('name:q','rpaframework' + "ENTER")
-    # 
+class BrowserManager:
+    def __init__(self):
+        self.browser = None
 
-#     <textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="false" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwie-97TmsCFAxVEzAIHHcFLBIEQ39UDCAQ"></textarea>
-# <textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="true" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwjUj-yLm8CFAxWP6wIHHc3QCioQ39UDCAQ" aria-activedescendant="" style=""></textarea>
-# //*[@id="APjFqb"]
-# <input class="gNO89b" value="Google Search" aria-label="Google Search" name="btnK" role="button" tabindex="0" type="submit" data-ved="0ahUKEwjUj-yLm8CFAxWP6wIHHc3QCioQ4dUDCBE">
-    # browser.configure(
-    #    browser_engine="chromium", 
-    #    screenshot="only-on-failure",
-    #    headless= False,
+    #oppening the site aljazeera.com
+    def opening_the_news_Site(self, url):
 
-    #    )
-    # page = browser.goto("https://www.google.co.uk")
-    # page.fill("//input[@name='q']", 'rpaframework')
-    # page.click("input:text('Google Search')")
+        # logger.info("Opening the news site.")
+        browser = Selenium(auto_close = False)
+        
+        # Define Chrome options to disable popup blocking
+        options = [
+            "--disable-popup-blocking",
+            "--ignore-certificate-errors"
+        ]
+
+        
+
+        # Open browser with specified options
+        browser.open_available_browser(url, 
+                                            browser_selection="Chrome", 
+                                            options=options)
+        # browser.close_all_browsers()
+        # return browser
+        print("oppend browser at last")
+
+
+    def search_the_phrase(self):
+        print("inside search method")
+        if(self.browser):
+            print("found browser")
+        else:
+            print("not found browser")
+        # logger.info(f"Searching the phrase: {phrase}")
+        # if the site contains collecting cookies 
+        try:
+            print("inside tyr")
+            sbrowser.click_button('Allow all')
+            print("it clicked allow")
+
+        except:
+            print("it didn't clicked allow")
+            pass
+        # finding the serach icon and field
+        locator1 = "//button[@aria-pressed='false']//*[name()='svg']"
+        browser.wait_until_page_contains_element(locator1, timeout=10)
+        browser.click_element(locator1)
+
+        # inserting the search phrase in the input field
+        browser.input_text("//input[@placeholder='Search']",phrase)
+        browser.click_button("//button[@aria-label='Search Al Jazeera']")
+        browser.close_all_browsers()
+
+
+        # Trying to find it there is a realated articles with the search phrase
+        try:
+            locator2 = "//select[@id='search-sort-option']"
+            browser.wait_until_element_is_visible(locator2, timeout=10)
+            browser.click_element(locator2)
+        except:
+            return "No news associated with the search phrase"
+
+        # sort by time
+        dropdown_locator = "//select[@id='search-sort-option']/option[1]" 
+        browser.click_element(dropdown_locator)
+
+bm = BrowserManager()
+url = "https://www.aljazeera.com/"
+bm.opening_the_news_Site(url)
+bm.search_the_phrase()
